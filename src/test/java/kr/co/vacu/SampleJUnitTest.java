a5c000814b55425c9b5e92a89e3380f65c86277d
cro575 <cro575@nate.com>
2013-07-04 01:42:01 +0900

    test

--- src/test/java/kr/co/vacu/SampleJUnitTest.java
+++ src/test/java/kr/co/vacu/SampleJUnitTest.java
@@ -1,10 +1,22 @@
 package kr.co.vacu;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SampleJUnitTest {
 	private int i = 1;
 
+	@Before
+	public void before() {
+		System.out.println("before");
+	}
+	
+	@After
+	public void after() {
+		System.out.println("before");
+	}
+	
 	@Test
 	public void test1() {
 		i++;
