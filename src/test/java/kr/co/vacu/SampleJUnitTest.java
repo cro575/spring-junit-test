40b9a132537f0a7284688700abaa7df781b85799
cro575 <cro575@nate.com>
2013-07-19 11:55:57 +0900

    cro575

--- src/test/java/kr/co/vacu/SampleJUnitTest.java
+++ src/test/java/kr/co/vacu/SampleJUnitTest.java
@@ -1,12 +1,25 @@
 package kr.co.vacu;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SampleJUnitTest {
-	private int i = 1;
+	private int i = 0;
 
+	@BeforeClass
+	static public void beforeClass() {
+		System.out.println("beforeClass");
+	}
+	
+	@AfterClass
+	static public void afterClass() {
+		System.out.println("afterClass");
+	}
+	
 	@Before
 	public void before() {
 		System.out.println("before");
@@ -14,7 +27,12 @@ public class SampleJUnitTest {
 	
 	@After
 	public void after() {
-		System.out.println("before");
+		System.out.println("after");
+	}
+
+	@Ignore
+	public void testIgnore() {
+		System.out.println("testIgnore");
 	}
 	
 	@Test
